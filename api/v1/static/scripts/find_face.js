f5197f449f289e99b3f2399df0cf866b563b5efb
fabo893 <jose.rosa893@gmail.com>
2021-07-30 18:25:53 -0400

    fixing and debugging

--- api/v1/static/scripts/find_face.js
+++ api/v1/static/scripts/find_face.js
@@ -1,47 +1,5 @@
 #!/usr/bin/nodejs
 
-/*
-const files = document.getElementById("img_one");
-const previewContainer = document.getElementById("img_one_preview");
-const previewImage = previewContainer.querySelector(".preview_image_one");
-const previewText = previewContainer.querySelector(".preview_text_one");
-
-$('#btn').on('click', function () {
-
-    const file = this.files[0];
-
-    $.ajax({
-        type: 'POST',
-        url: '/find',
-        data: file,
-        sucess: function() {
-            if (file) {
-                const reader = new FileReader();
-                previewText.style.display = "none";
-                previewImage.style.display = "block";
-                reader.addEventListener("load", function () {
-                  console.log(this);
-                  previewImage.setAttribute("src", this.result);
-                });
-                reader.readAsDataURL(file);
-            }
-        },
-
-    });
-
-});
-*/
-
-$('#btn').on('click', function () {
-    const img1 = document.querySelector(".preview_image_one");
-    const img2 = document.querySelector(".preview_image_two");
-
-    $.ajax({
-        type: 'POST',
-        url: '/find',
-        data: { param: img1, param2: img2 },
-        success: function () {
-            console.log("Good")
-        }
-    });
+$("IMG.preview_img_one").onclick(function() {
+    $(this).css('width', '50%');
 });
