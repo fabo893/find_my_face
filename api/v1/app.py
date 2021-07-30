b0a911560ea7682b6e4c3c788747b18cddb558df
fabo893 <jose.rosa893@gmail.com>
2021-07-30 05:46:01 +0000

    rendering templates

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,14 +2,13 @@
 """
 Api for Find My Face
 """
-from flask import Flask, jsonify
-from models import storage
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.errorhandler(404)
-def page_not_found(exception=None):
-    return jsonify({"error": "Not found"}), 404
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', '5000', threaded=True)
+    app.run(debug=True)
