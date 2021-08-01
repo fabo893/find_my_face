31fa0d0da82568388c76fb4512632254ebd0431d
fabo893 <jose.rosa893@gmail.com>
2021-08-01 05:19:13 -0400

    try

--- api/v1/static/scripts/find_face.js
+++ api/v1/static/scripts/find_face.js
@@ -1,9 +1,5 @@
-#!/usr/bin/nodejs
 
 $("#btn").click(function() {
-    let img_one = (".preview_image_one").attr("src");
-    let img_two = (".preview_image_two").attr("src");
 
-    console.log(img_one);
-    console.log(img_two);
+    console.log("Working!!!");
 });
