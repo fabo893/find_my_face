d6d0cdfc51601caded49df0e32c13818d82f2134
fabo893 <jose.rosa893@gmail.com>
2021-08-02 13:29:47 -0400

    try

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,10 +5,9 @@ Api for Find My Face
 
 from os import name
 import json
-from werkzeug.utils import redirect
 from models.known import Known
 from models.unknown import Unknown
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, redirect, request, url_for
 
 
 app = Flask(__name__)
@@ -17,15 +16,16 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/display/<dic>')
+def display(dic):
+    return render_template('test.htlm', dic=dic)
+
 @app.route("/upload", methods=['POST'])
 def upload():
     test1 = request.get_json()
     json_parse = json.dumps(test1)
     return redirect(url_for('display', dic = json_parse))
 
-@app.route('/display/<dic>')
-def display(dic):
-    return render_template('test.htlm', dic=dic)
     
 
 if __name__ == "__main__":
