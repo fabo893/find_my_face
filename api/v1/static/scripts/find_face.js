5dba96cfc52b141c000316461af7b8088de1e455
fabo893 <jose.rosa893@gmail.com>
2021-08-07 19:29:11 -0400

    try

--- api/v1/static/scripts/find_face.js
+++ api/v1/static/scripts/find_face.js
@@ -4,11 +4,4 @@ $("#btn").click(function () {
     let img1 = $(".preview_image_one").attr("src");
     let img2 = $('.preview_image_two').attr('src');
 
-    $.ajax({
-        type: 'POST',
-        url: '~/testing/printMatch.py' + img1 + img2,
-        success: function (response) {
-            console.log(response);
-        }
-    });
-});
+})
--- api/v1/static/scripts/image_preview.js
+++ api/v1/static/scripts/image_preview.js
@@ -12,7 +12,7 @@ files.addEventListener("change", function () {
     previewText.style.display = "none";
     previewImage.style.display = "block";
     reader.addEventListener("load", function () {
-      console.log(this);
+      console.log(this.result);
       previewImage.setAttribute("src", this.result);
     });
     reader.readAsDataURL(file);
