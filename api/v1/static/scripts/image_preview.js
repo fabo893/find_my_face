0e346a0027e9966a7fc37eb456083d1b7a74f19f
fabo893 <jose.rosa893@gmail.com>
2021-08-07 19:46:37 -0400

    try

--- api/v1/static/scripts/image_preview.js
+++ api/v1/static/scripts/image_preview.js
@@ -7,7 +7,7 @@ const previewText = previewContainer.querySelector(".preview_text_one");
 
 files.addEventListener("change", function () {
   const file = this.files[0];
-  console.log(file.name);
+  console.log(typeof(file.name));
 
   if (file) {
     const reader = new FileReader();
