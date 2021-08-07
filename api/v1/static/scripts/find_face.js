ad7f93bc4bd6fbed0e2c4636936fc0041296f230
fabo893 <jose.rosa893@gmail.com>
2021-08-06 22:24:17 -0400

    try

--- api/v1/static/scripts/find_face.js
+++ api/v1/static/scripts/find_face.js
@@ -3,7 +3,7 @@
 $("#btn").click(function () {
     let img1 = $(".preview_image_one").attr("src");
 
-    let img = $('#upload').append('<img>');
+    $('#upload').append('<img id="subir">');
 
-    img.attr('src');
+    $('#subir').attr('src=' + img1);
 });
