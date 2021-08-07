1bc3bed92ef2017b089a959f4301ddd875d663ad
fabo893 <jose.rosa893@gmail.com>
2021-08-06 22:07:56 -0400

    TRY

--- api/v1/static/scripts/find_face.js
+++ api/v1/static/scripts/find_face.js
@@ -3,17 +3,5 @@
 $("#btn").click(function () {
     let img1 = $(".preview_image_one").attr("src");
 
-    json_img = {'name': 'testing1', 'image': img1};
-
-    json_par = JSON.stringify(json_img);
-
-    $.ajax({
-        type: 'POST',
-        url: '/upload',
-        data: json_par,
-        contentType: 'application/json',
-        success: function() {
-            console.log("Good");
-        }
-    });
+    $('#upload').append('<img>');
 });
