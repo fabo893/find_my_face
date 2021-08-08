d197175f237e3dc59a44ff771a1143c212384314
fabo893 <jose.rosa893@gmail.com>
2021-08-08 02:28:02 -0400

    try

--- api/v1/static/scripts/find_face.js
+++ api/v1/static/scripts/find_face.js
@@ -4,22 +4,25 @@ $("#btn").click(function () {
     testing();
 })
 
-let json = {
-    name: 'Fabo',
-    email: 'jose@gmail.com',
-    estado: 'soltero'
-}
+function testing(){
 
-$.ajax({
-    type: 'POST',
-    url: '/upload',
-    data: json,
-    success: function (response) {
-        if (response === true) {
-            console.log('Lo he logrado');
-        }
-        else {
-            console.log('No lo logre');
-        }
+    let json = {
+        name: 'Fabo',
+        email: 'jose@gmail.com',
+        estado: 'soltero'
     }
-});
+    
+    $.ajax({
+        type: 'POST',
+        url: '/upload',
+        data: json,
+        success: function (response) {
+            if (response === true) {
+                console.log('Lo he logrado');
+            }
+            else {
+                console.log('No lo logre');
+            }
+        }
+    });
+}
