19dfcb5dd7a2afda598cbb036c167c6ce2058f4f
fabo893 <jose.rosa893@gmail.com>
2021-08-10 22:49:24 -0400

    try

--- models/known.py
+++ models/known.py
@@ -5,16 +5,15 @@
 
 import models
 from models.base_model import BaseModel, Base
-from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String, BLOB, ForeignKey
+from sqlalchemy import Column, String, BLOB, ForeignKey, Text
 
 
 class Known(BaseModel, Base):
     """ Representation of known picture """
     __tablename__ = 'known_img'
     name = Column(String(128), nullable=False)
-    image = Column(BLOB)
+    image = Column(Text, nullable=False)
     type = Column(String(15), nullable=False)
 
 
