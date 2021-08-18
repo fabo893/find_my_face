048f3386aa72b81643e57a397b2c817304fe3b8e
fabo893 <jose.rosa893@gmail.com>
2021-08-18 00:22:26 -0400

    try

--- api/v1/static/scripts/find_face.js
+++ api/v1/static/scripts/find_face.js
@@ -4,6 +4,7 @@ $("#btn").click(function () {
     let imagen_uno = document.getElementById('img_one');
     let file_uno = imagen_uno.files[0];
     let src = document.getElementById('test_uno').getAttribute('src');
+    let srcSub = src.substr(23);
 
     let imagen_dos = document.getElementById('img_two');
     let file_dos = imagen_dos.files[0];
@@ -19,7 +20,7 @@ $("#btn").click(function () {
         size: file_uno.size,
         lastModified: file_uno.lastModified,
         lasModifiedDate: file_uno.lasModifiedDate,
-        source: src
+        source: srcSub
     };
 
     const newImage = JSON.stringify(newObj);
